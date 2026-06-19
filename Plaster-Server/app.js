2bfb4cc5c0caebb12c182d1b0abcd8d77b23f7f0
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2026-06-19 01:27:02 -0500

    test

--- Plaster-Server/app.js
+++ Plaster-Server/app.js
@@ -3,20 +3,9 @@ const app = express();
 const port = 2150;
 
 app.get('/connect', (req, res) => {
-  console.log(req.query);
-  if (req.query.token === '12345') {
-    res.send('Connected');
-  } else {
-    res.status(401).send('Unauthorized');
-  }
+  
 });
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
@@ -25,4 +14,7 @@ app.listen(port, () => {
 while (true) {
     // keep server running
     task.sleep(1000);
+}
+
+while (true) {//testing hackatype currently.
 }
