80a16eb67208092f45683d53994961562cc4fd3e
The Wrench <123601564+TateTheBait@users.noreply.github.com>
2026-06-19 01:23:44 -0500

    Create Node App

---
+++ Plaster-Server/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 2150;
+
+app.get('/connect', (req, res) => {
+  console.log(req.query);
+  if (req.query.token === '12345') {
+    res.send('Connected');
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
